de1cc909bd11bc32e4e48d5587e465a5f36dbd84
volgakurvar <kurvan1112@gmail.com>
2024-02-24 01:45:38 +0900

    初期スクリプト追加

--- lib/aws-cdk-example-stack.ts
+++ lib/aws-cdk-example-stack.ts
@@ -5,10 +5,34 @@ import {
   InstanceType,
   LaunchTemplate,
   MachineImage,
+  Peer,
+  Port,
+  SecurityGroup,
   SubnetType,
+  UserData,
   Vpc,
 } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
+
+const GAME_NAME = "Palworld";
+const APP_ID = 2394010;
+const EXECUTABLE = "PalServer.sh";
+const PORT: Port[] = [Port.udp(8211)];
+const INSTANCE_TYPE = "t3a.large";
+const VOLUME_SIZE = 16;
+
+const service = `[Unit]
+Description=Game server
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/home/ec2-user/${GAME_NAME}/${EXECUTABLE}
+Restart=always
+User=ec2-user
+
+[Install]
+WantedBy=default.target`.replace("\n", "\\n");
 
 export class GameServerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -24,24 +48,47 @@ export class GameServerStack extends Stack {
       ],
     });
 
+    const securityGroup = new SecurityGroup(this, "SecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
+    });
+    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
+    PORT.forEach((port) => securityGroup.addIngressRule(Peer.anyIpv4(), port));
+
     const launchTemplate = new LaunchTemplate(this, "Template", {
       spotOptions: {},
     });
 
+    const userData = UserData.forLinux();
+    userData.addCommands(
+      "yum update -y",
+      "yum install -y glibc.i686 libstdc++48.i686",
+      "cd /home/ec2-user",
+      "wget 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz'",
+      "tar -xzvf steamcmd_linux.tar.gz",
+      "rm -f steamcmd_linux.tar.gz",
+      "find -exec chmod 777 {} \\;",
+      `./steamcmd.sh +force_install_dir ${GAME_NAME} +login anonymous +app_update ${APP_ID} validate +quit`,
+      `echo -e "${service}" > /etc/systemd/system/game.service`,
+      "systemctl daemon-reload",
+      "systemctl start game",
+    );
     const instance = new Instance(this, "EC2", {
       vpc,
-      instanceType: new InstanceType("t3a.large"),
+      instanceType: new InstanceType(INSTANCE_TYPE),
       machineImage: MachineImage.latestAmazonLinux2(),
+      securityGroup,
       blockDevices: [
         {
           deviceName: "/dev/xvda",
           volume: {
             ebsDevice: {
-              volumeSize: 16,
+              volumeSize: VOLUME_SIZE,
             },
           },
         },
       ],
+      userData,
     });
     instance.instance.launchTemplate = {
       version: launchTemplate.versionNumber,

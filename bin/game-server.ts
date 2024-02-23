6df2369453c5311b6f3c82bc2641f4e9ddbb782e
volgakurvar <kurvan1112@gmail.com>
2024-02-24 01:50:21 +0900

    リネーム

--- bin/game-server.ts
+++ bin/game-server.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { GameServerStack } from "../lib/aws-cdk-example-stack";
+import { GameServerStack } from "../lib/game-server-stack";
 
 const app = new cdk.App();
 new GameServerStack(app, "GameServerStack");

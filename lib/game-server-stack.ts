fc9e483dd856425a75fde229055de08a165bee35
yuto.moriizumi <kurvan1112@gmail.com>
2024-08-21 17:46:32 +0900

    ユニットテストとgithub workflowを追加

--- lib/game-server-stack.ts
+++ lib/game-server-stack.ts
@@ -122,3 +122,4 @@ export class GameServerStack extends Stack {
     });
   }
 }
+
--- test/aws-cdk-example.test.ts
+++ test/aws-cdk-example.test.ts
@@ -2,15 +2,12 @@ import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import * as AwsCdkExample from "../lib/game-server-stack";
 
-test("SQS Queue and SNS Topic Created", () => {
+test("VPC and EC2 instance created", () => {
   const app = new cdk.App();
 
   const stack = new AwsCdkExample.GameServerStack(app, "MyTestStack");
 
   const template = Template.fromStack(stack);
-
-  template.hasResourceProperties("AWS::SQS::Queue", {
-    VisibilityTimeout: 300,
-  });
-  template.resourceCountIs("AWS::SNS::Topic", 1);
+  template.resourceCountIs("AWS::EC2::VPC", 1);
+  template.resourceCountIs("AWS::EC2::Instance", 1);
 });
